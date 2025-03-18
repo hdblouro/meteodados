84777a112e3af369cc56f5b107b6fb3be482fcf8
hdblouro <henrique.louro@terra.com.br>
2025-03-18 08:29:55 -0300

    Início Ex 02

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import Meteo from "./models/Meteo";
 
-const meteo = Array<Meteo>(); // cria um array de objetos da classe Meteo vazio
+var meteo = Array<Meteo>(); // cria um array de objetos da classe Meteo vazio
 
 var x: number = 0; // variável necessária para pular a primeira linha de cabeçalho do arquivo CSV
 
@@ -12,58 +12,90 @@ const data = fs.readFileSync('./src/Meteo.csv',
 data.forEach(linha => { // faz a leitura de cada linha da variável data
     if (x > 0) { // só processa se não for a primeira linha
         var l = linha.split(';'); // quebra a linha nos pontos-e-vírgula gerando um array com cada campo/coluna
-        //        console.log(l);
+        // console.log(l);
         let ano: number = parseInt(l[0].substring(6, 10));
         let mes: number = parseInt(l[0].substring(3, 5)) - 1;
         let dia: number = parseInt(l[0].substring(0, 2));
         const m = new Meteo(
-            new Date(ano,mes,dia),
+            new Date(ano, mes, dia),
             l[1],
-            parseFloat(l[2].replace(",",".")), // troca a virgula por ponto no valor e converte de string para number
-            parseFloat(l[3].replace(",",".")),
-            parseFloat(l[4].replace(",",".")),
-            parseFloat(l[5].replace(",",".")),
-            parseFloat(l[6].replace(",",".")),
-            parseFloat(l[7].replace(",",".")),
-            parseFloat(l[8].replace(",",".")),
-            parseFloat(l[9].replace(",",".")),
-            parseFloat(l[10].replace(",",".")),
-            parseFloat(l[11].replace(",",".")),
-            parseFloat(l[12].replace(",","."))); // instancia um objeto do Modelo a ser usado
+            parseFloat(l[2].replace(",", ".")), // troca a virgula por ponto no valor e converte de string para number
+            parseFloat(l[3].replace(",", ".")),
+            parseFloat(l[4].replace(",", ".")),
+            parseFloat(l[5].replace(",", ".")),
+            parseFloat(l[6].replace(",", ".")),
+            parseFloat(l[7].replace(",", ".")),
+            parseFloat(l[8].replace(",", ".")),
+            parseFloat(l[9].replace(",", ".")),
+            parseFloat(l[10].replace(",", ".")),
+            parseFloat(l[11].replace(",", ".")),
+            parseFloat(l[12].replace(",", "."))); // instancia um objeto do Modelo a ser usado
         meteo.push(m); // adiciona o objeto m ao array de objetos meteo
-        console.log(m);
+//        console.log(m);
     }
     x++; // incrementa a varíavel de controle de linha
 }); // fecha data.forEach
 
-console.log(meteo.length);
-console.log(x);
+//console.log(meteo.length);
+//console.log(x);
 
-/*
 // Resolução do Exercício C
-// Informar quais as 6 dezenas mais sorteadas, inclusive a quantidade de vezes. 
-// Se ocorrer empate, listar todas na ordem de classificação;
+// Informar quais os 5 dias, que tiveram as mais altas temperaturas. 
+// Se ocorrer empate, listar todas na ordem por dia;
 
-var dezenas: number[][] = [];
+let medias:Meteo[] = Array<Meteo>();
+let dia:Date = meteo[0].Data;
+let cont:number = 0;
+x = 0;
+let meteodia:Meteo = new Meteo(dia, "00:00", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-for (x = 0; x < 60; x++) {
-    dezenas[x] = [];
-    dezenas[x].push(x+1);
-    dezenas[x].push(0);
+while (x < meteo.length) {
+    while (true) {
+        meteodia.Temp_C += meteo[x].Temp_C;
+        meteodia.Hum += meteo[x].Hum;
+        meteodia.Press_Bar += meteo[x].Press_Bar;
+        meteodia.TempCabine_C += meteo[x].TempCabine_C;
+        meteodia.Charge += meteo[x].Charge;
+        meteodia.SR_Wm2 += meteo[x].SR_Wm2;
+        meteodia.WindPeak_ms += meteo[x].WindPeak_ms;
+        meteodia.WindSpeed_Inst += meteo[x].WindDir_Inst;
+        meteodia.WindSpeed_Avg += meteo[x].WindSpeed_Avg;
+        meteodia.WindDir_Inst += meteo[x].WindDir_Inst;
+        meteodia.WindDir_Avg += meteo[x].WindDir_Avg;
+        cont++;
+        x++;
+        if (x == meteo.length) {
+            break;
+        } else {
+            if (dia.getTime() !== meteo[x].Data.getTime()){
+                break;
+            }
+        }
+    }
+    
+    meteodia.Temp_C /= cont;
+    meteodia.Hum /= cont;
+    meteodia.Press_Bar /= cont;
+    meteodia.TempCabine_C /= cont;
+    meteodia.Charge /= cont;
+    meteodia.SR_Wm2 /= cont;
+    meteodia.WindPeak_ms /= cont;
+    meteodia.WindSpeed_Inst /= cont;
+    meteodia.WindSpeed_Avg /= cont;
+    meteodia.WindDir_Inst /= cont;
+    meteodia.WindDir_Avg /= cont;
+    medias.push(meteodia);
+
+    if(x<meteo.length){
+        dia = meteo[x].Data;
+        cont = 0;
+        meteodia = new Meteo(dia, "00:00", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    }
 }
 
-mega.forEach(m => { // faz a leitura de cada objeto Mega do array mega colocando-o em m
-    //    console.log("Concurso:", m.concurso);
-    dezenas[m.dez1 - 1][1]++; // somatório da 1ª dezena sorteada na posição específica do array
-    dezenas[m.dez2 - 1][1]++; // somatório da 2ª dezena sorteada na posição específica do array
-    dezenas[m.dez3 - 1][1]++; // somatório da 3ª dezena sorteada na posição específica do array
-    dezenas[m.dez4 - 1][1]++; // somatório da 4ª dezena sorteada na posição específica do array
-    dezenas[m.dez5 - 1][1]++; // somatório da 5ª dezena sorteada na posição específica do array
-    dezenas[m.dez6 - 1][1]++; // somatório da 6ª dezena sorteada na posição específica do array
-}); // fecha mega.forEach
+console.log(medias);
 
-dezenas.sort(sort_columnWise);
-
+/*
 // Callback function to sort array according to the third column.
 // If callback function will return 1, sort() method swap the row, Otherwise not.
 function sort_columnWise(
@@ -81,15 +113,26 @@ function sort_columnWise(
     }
 }
 
+function compare(a:Meteo,b:Meteo) {
+    if (a.Temp_C < b.Temp_C)
+       return -1;
+    if (a.Temp_C > b.Temp_C)
+      return 1;
+    return 0;
+  }
+
+medias.sort(compare);
+
 var y = 0;
-var ant = dezenas[0][0];
+var ant = medias[0].Data;
 console.log("<< Exercício C >>\n");
-for (x = 0; x < 60; x++) {
-    console.log("Dezena", dezenas[x][0] , ":", dezenas[x][1]);
-    if(ant != dezenas[x][0]){
+for (x = 0; x < medias.length; x++) {
+    console.log("Dia:", medias[x].Data , "Temperatura Média: ", medias[x].Temp_C);
+    if(ant.getTime() != medias[x].Data.getTime()){
         y++;
+        ant = medias[x].Data;
     }
-    if(y==6){
+    if(y==5){
         break;
     }
 }
